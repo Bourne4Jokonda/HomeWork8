d3a33a2d99f040587a647b7a06ec57e644b08525
«Aleksandr» <bourne4jokonda@gmail.com>
2022-07-18 09:38:33 +0300

    need help task 54

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -1,2 +1,43 @@
 ﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит
 // по убыванию элементы каждой строки двумерного массива
+
+int[,] massive = new int[3, 4];
+for (int i = 0; i < massive.GetLength(0); i++)
+{
+      for (int j = 0; j < massive.GetLength(1); j++)
+      {
+            massive[i, j] = new Random().Next(1, 10);
+            Console.Write(massive[i, j] + " ");
+      }
+      Console.WriteLine();
+}
+Console.WriteLine();
+
+void BubbleSort(int[,] arr)
+      {
+	for (int i = 0; i < arr.GetLength(0); i++)
+		for (int j = 0; j < arr.GetLength(1) - 1; j++)
+			if (arr[i,j] > arr[i,j + 1])
+				{
+                              int max = arr[i,j];
+					arr[i,j] = arr[i,j+1];
+					arr[i,j+1] = max;
+                              Console.WriteLine(arr[i,j]);
+				}
+                  else
+                  {
+                        j++;
+                  }
+	}
+ 
+BubbleSort(massive);
+
+for (int i = 0; i < massive.GetLength(0); i++)
+{
+      for (int j = 0; j < massive.GetLength(1); j++)
+      {
+            Console.Write(massive[i, j] + " ");
+      }
+      Console.WriteLine();
+}
+Console.WriteLine();
