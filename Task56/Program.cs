3b9dc6dde6d0b8d5388daa3d39d7f4e510300b92
«Aleksandr» <bourne4jokonda@gmail.com>
2022-07-19 10:27:13 +0300

    Need help with Task 56

--- Task56/Program.cs
+++ Task56/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Задача 56: Задайте прямоугольный двумерный массив.
 // Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 int[,] massive = new int[4, 3];
-int []sum=new int [massive.GetLength(0)];
-int LineSum=0;
+int []sum=new int [massive.GetLength(1)];
+int sumset =0;
 for (int i = 0; i < massive.GetLength(0); i++)
 {
       for (int j = 0; j < massive.GetLength(1); j++)
@@ -18,17 +18,16 @@ for (int i = 0; i < massive.GetLength(0); i++)
 {
       for (int j = 0; j < massive.GetLength(1); j++)
       {
-            int sums = 0;
-            sums = sums + massive[i, j];
-            sum[i]=sums;
-            Console.Write(sum[i]);
-            sums =0;
+         sum[i]=sum[i]+massive[i,j];   
       }
+}
 
-Console.WriteLine();
-}
-LineSum(int arr[a])
-for (int i = 0; i < sum.Length; i++)
-{
-      LineSum=LineSum+sum[i];
-}
+for (int i = 1; i <= sum.Length; i++)
+      {
+      int min=sum[i-1];
+      if(sum[i-1]<sum[i])
+      {
+            sum[i-1]=min;
+      }
+      Console.WriteLine($"Строка {i} с суммой {min} является наименьшей строкой");
+      }
