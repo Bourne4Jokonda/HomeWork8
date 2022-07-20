834edae8cf225f12e19a870961c8c3ae6df9483b
«Aleksandr» <bourne4jokonda@gmail.com>
2022-07-20 09:48:14 +0300

    Task 56 complete

--- Task56/Program.cs
+++ Task56/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Задача 56: Задайте прямоугольный двумерный массив.
 // Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 int[,] massive = new int[4, 3];
-int []sum=new int [massive.GetLength(1)];
-int sumset =0;
+int[] sum = new int[massive.GetLength(0)];
+
 for (int i = 0; i < massive.GetLength(0); i++)
 {
       for (int j = 0; j < massive.GetLength(1); j++)
@@ -18,16 +18,24 @@ for (int i = 0; i < massive.GetLength(0); i++)
 {
       for (int j = 0; j < massive.GetLength(1); j++)
       {
-         sum[i]=sum[i]+massive[i,j];   
+            sum[i] = sum[i] + massive[i, j];
+            Console.Write(sum[i]+" ");
       }
+      Console.WriteLine();
 }
 
-for (int i = 1; i <= sum.Length; i++)
-      {
-      int min=sum[i-1];
-      if(sum[i-1]<sum[i])
-      {
-            sum[i-1]=min;
-      }
-      Console.WriteLine($"Строка {i} с суммой {min} является наименьшей строкой");
-      }
+int MinElement(int[] array)
+        {
+            int minElement = array[0];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                    if (minElement > array[i])
+                    {
+                        minElement = array[i];
+                    }
+            }
+            // Console.WriteLine(minElement);
+            Console.WriteLine($"Строка с суммой {minElement} является наименьшей строкой");
+            return minElement;
+        }
+MinElement(sum);
